e701d7f40b9afc52efa8b1e3032382d649af7898
liruibin <liruibin@lvjinsuo.com>
2017-10-26 09:49:11 +0800

    redux test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom'
+import Home from './containers/Home'
+import About from './containers/About'
 
 class App extends Component {
   render() {
@@ -9,10 +12,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about-us">About</Link>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+        </main>
       </div>
     );
   }
---
+++ src/containers/About/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by robin on 17/10/5.
+ */
+
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>Did you get here via Redux?</p>
+  </div>
+)
+
+export default About
